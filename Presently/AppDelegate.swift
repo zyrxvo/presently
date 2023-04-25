1b5cb652503b0be935915d52dad9af2bb22818fa
Garett Brown <zyrxvo.us@gmail.com>
2023-04-25 13:45:54 -0400

    Initial Commit

---
+++ Presently/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Presently
+//
+//  Created by Garett Brown on 2023-04-25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Presently/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Presently
+//
+//  Created by Garett Brown on 2023-04-25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
