a612e69346f90049c44a080c71fcac79d263cf8b
Garett Brown <zyrxvo.us@gmail.com>
2023-04-27 15:24:14 -0400

    Updated Icon. Programatically hide dock icon mostly working.

--- Presently/AppDelegate.swift
+++ Presently/AppDelegate.swift
@@ -12,43 +12,98 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     let defaults = UserDefaults.standard
     let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.variableLength)
     var timer = Timer()
+    var dockIconMenuItem = NSMenuItem(title: "Hide Dock Icon", action: #selector(toggleDockIcon), keyEquivalent: "d")
+    var repeatingTimer = Timer()
     var counter = 0
-    var sleepTimer = 0
+    var sleepTimer = -100
     var windowController: NSWindowController?
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-
-        if let button = statusItem.button {
+        defaults.synchronize()
+        let iconIsHidden = defaults.bool(forKey: "hideDockIcon")
+        if iconIsHidden { NSApp.setActivationPolicy(.accessory) }
+        else { NSApp.setActivationPolicy(.regular) }
+        
+        guard let button = statusItem.button else { return }
             
-            button.title = "Presently"
-            button.action = #selector(showMenu(_:))
-            
-            updateTimer()
-            self.timer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(self.updateTimer), userInfo: nil, repeats: true)
-            self.timer.tolerance = 5
-            RunLoop.current.add(self.timer, forMode: .common)
-//            self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.count), userInfo: nil, repeats: true)
-//            RunLoop.current.add(self.timer, forMode: .common)
+        button.title = "Presently"
+        button.action = #selector(showMenu(_:))
+        
+        Task.detached {
+            let interval = await self.update()
         }
+            
+//            updateTimer()
+//            print("\(self.sleepTimer)")
+//            self.timer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(self.updateTimer), userInfo: nil, repeats: true)
+//            self.timer.tolerance = 5
+//            RunLoop.current.add(self.timer, forMode: .common)
+//            self.initialTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.count), userInfo: nil, repeats: true)
+//            RunLoop.current.add(self.initialTimer, forMode: .common)
+    }
+    
+    @objc func test() {
+        print("Executed!")
+        print("\(self.sleepTimer)")
+    }
+    
+    @objc func count() {
+        counter += 1
+        print(":-> \(counter)")
+    }
+    
+    @objc func toggleDockIcon() {
+        defaults.synchronize()
+        let iconIsHidden = defaults.bool(forKey: "hideDockIcon")
+        if iconIsHidden {
+            NSApp.setActivationPolicy(.regular)
+            NSApp.activate(ignoringOtherApps: true)
+            defaults.set(false, forKey: "hideDockIcon")
+            self.dockIconMenuItem.title = "Hide Dock Icon"
+        }
+        else {
+            NSApp.setActivationPolicy(.accessory)
+            defaults.set(true, forKey: "hideDockIcon")
+            self.dockIconMenuItem.title = "Show Dock Icon"
+        }
+        defaults.synchronize()
     }
     
     @objc func showMenu(_ sender: Any?) {
+        print(self.dockIconMenuItem.title)
         let menu = NSMenu()
         menu.addItem(NSMenuItem(title: "Refresh", action: #selector(self.updateTimer), keyEquivalent: "r"))
+        menu.addItem(self.dockIconMenuItem)
         menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
         statusItem.menu = menu
         statusItem.button?.performClick(nil)
     }
     
-    @objc func count() {
+    @objc func update() async -> Int {
         counter += 1
-        print(counter)
+        print("Update \(counter)")
+        timer.invalidate()
+        var interval = 0
+        do {
+            let result = try await query2()
+            if let button = statusItem.button {
+                DispatchQueue.main.async {
+                    button.title = result.0
+                }
+            }
+            interval = result.1
+        } catch {
+            print("Error")
+        }
+        return interval
     }
     
     @objc func updateTimer() {
         if let button = statusItem.button {
             query { currentTimer, sleepInterval in
-                self.sleepTimer = sleepInterval ?? 0
+//                self.sleepTimer = sleepInterval ?? 0
+//                print("Processed: \(sleepInterval ?? -5)")
+//                self.initialTimer = Timer.scheduledTimer(timeInterval: TimeInterval(sleepInterval ?? 0), target: self, selector: #selector(self.test), userInfo: nil, repeats: false)
                 if let currentTimer = currentTimer {
                     DispatchQueue.main.async {
                         button.title = currentTimer
@@ -56,10 +111,48 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                 } else {
                     print("Error: Unable to retrieve current timer")
                 }
+                if let sleepInterval = sleepInterval {
+                    if sleepInterval < 60 {
+                        print("S: \(sleepInterval)")
+                        DispatchQueue.main.asyncAfter(deadline: .now() + TimeInterval(sleepInterval - 1)) {
+                            print("Timer fired!")
+                            self.repeatingTimer = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(self.updateTimer), userInfo: nil, repeats: true)
+                            RunLoop.current.add(self.repeatingTimer, forMode: .common)
+                        }
+                    }
+                }
+                else {
+                    print("Error: No time interval given")
+                }
             }
         }
     }
 
+//    @objc func showSettingsWindow() {
+//        if let window = NSApplication.shared.windows.first(where: { $0.identifier?.rawValue == "Presently" }) {
+//            // The settings window is already open, so just bring it to the front
+//            window.makeKeyAndOrderFront(nil)
+//        } else {
+//            // The settings window is not open, so create and show a new instance
+//            let storyboard = NSStoryboard(name: "Main", bundle: nil)
+//            let settingsWindowController = storyboard.instantiateController(withIdentifier: "ViewController") as! NSWindowController
+//            settingsWindowController.showWindow(nil)
+//        }
+//    }
+    @objc func showSettingsWindow() {
+        if let window = NSApplication.shared.windows.first(where: { $0.identifier?.rawValue == "Presently" }) {
+            // The settings window is already open, so just bring it to the front
+            window.makeKeyAndOrderFront(nil)
+        } else {
+            // The settings window is not open, so create and show a new instance
+            let storyboard = NSStoryboard(name: "Main", bundle: nil)
+            if let settingsWindowController = storyboard.instantiateInitialController() as? NSWindowController {
+                settingsWindowController.showWindow(nil)
+            }
+        }
+    }
+
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
--- Presently/Caller.swift
+++ Presently/Caller.swift
@@ -82,3 +82,66 @@ func times(startingTime: String) -> (displayString: String, sleepTime: Int) {
     
     return (displayString: durstr, sleepTime: sleeptime)
 }
+
+func fetch(url: URL, request: URLRequest) async throws -> Data {
+    let config = URLSessionConfiguration.default
+    config.shouldUseExtendedBackgroundIdleMode = false
+    let session = URLSession(configuration: config)
+
+    do {
+        let (data, response) = try await session.data(for: request)
+        guard let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else {
+            print("Error: Non-200 status code returned")
+            return Data()
+        }
+        // process data
+        return data
+    } catch {
+        print("Error: \(error)")
+    }
+    return Data()
+}
+
+
+func query2() async throws -> (String, Int) {
+    let defaults = UserDefaults.standard
+    defaults.synchronize()
+    guard let apiKey = defaults.string(forKey: "APIKey") else { throw NSError(domain: "Missing API Key", code: 0, userInfo: nil) }
+    guard let url = URL(string: "https://api.track.toggl.com/api/v9/me/time_entries/current") else { throw NSError(domain: "Invalid URL", code: 0, userInfo: nil)}
+    
+    let loginData = "\(apiKey):api_token".data(using: .utf8)!
+    let base64LoginData = loginData.base64EncodedString()
+    
+    var request = URLRequest(url: url)
+    request.httpMethod = "GET"
+    request.setValue("Basic \(base64LoginData)", forHTTPHeaderField: "Authorization")
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    
+    var currentTimer = "Test"
+    do {
+        let data = try await fetch(url: url, request: request)
+        do {
+            if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                var name : String
+                guard let description = json["description"] as? String else { throw NSError(domain: "Toggl entry is missing description", code: 0, userInfo: nil) }
+                guard let tags = json["tags"] as? [String] else { throw NSError(domain: "Toggl entry is missing tags", code: 0, userInfo: nil) }
+                if tags.isEmpty { name = description}
+                else { name = tags[0] }
+                
+                guard let startingTime = json["start"] as? String else { throw NSError(domain: "Toggl entry is missing start time", code: 0, userInfo: nil) }
+                let result = times(startingTime: startingTime)
+                
+                currentTimer = "\(name) \(result.displayString)"
+                return (currentTimer, result.sleepTime)
+            } else {
+                throw NSError(domain: "Invalid JSON format", code: 0, userInfo: nil)
+            }
+        } catch let error as NSError {
+            print("Error: \(error.localizedDescription)")
+        }
+    } catch {
+        // handle error
+    }
+    
+    return ("Failed", 0)
+}
